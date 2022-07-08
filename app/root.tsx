5a1d42ca56cfd5c219ff05fc65a2586d33a13111
Corin McHargue <corinty@users.noreply.github.com>
2022-07-08 09:31:54 -0500

    Remove session server

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,7 @@
 import { useContext } from "react";
 import { json } from "@remix-run/node";
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import { withEmotionCache } from "@emotion/react";
 import { unstable_useEnhancedEffect as useEnhancedEffect } from "@mui/material";
 import theme from "./src/theme";
@@ -21,17 +10,17 @@ import ClientStyleContext from "./src/ClientStyleContex";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import globalStyles from "./styles/global.css";
 
-import { getUser } from "./session.server";
+// import { getUser } from "./session.server";
 
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
+// type LoaderData = {
+// user: Awaited<ReturnType<typeof getUser>>;
+// };
 
-export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
-};
+// export const loader: LoaderFunction = async ({ request }) => {
+// return json<LoaderData>({
+// user: await getUser(request),
+// });
+// };
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: globalStyles }];
@@ -48,56 +37,51 @@ interface DocumentProps {
   title?: string;
 }
 
-const Document = withEmotionCache(
-  ({ children, title }: DocumentProps, emotionCache) => {
-    const clientStyleData = useContext(ClientStyleContext);
+const Document = withEmotionCache(({ children, title }: DocumentProps, emotionCache) => {
+  const clientStyleData = useContext(ClientStyleContext);
 
-    // Only executed on client
-    useEnhancedEffect(() => {
-      // re-link sheet container
-      emotionCache.sheet.container = document.head;
-      // re-inject tags
-      const tags = emotionCache.sheet.tags;
-      emotionCache.sheet.flush();
-      tags.forEach((tag) => {
-        // eslint-disable-next-line no-underscore-dangle
-        (emotionCache.sheet as any)._insertTag(tag);
-      });
-      // reset cache to reapply global styles
-      clientStyleData.reset();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  // Only executed on client
+  useEnhancedEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      // eslint-disable-next-line no-underscore-dangle
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData.reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          {title ? <title>{title}</title> : null}
-          <Meta />
-          <Links />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <meta
-            name="emotion-insertion-point"
-            content="emotion-insertion-point"
-          />
-        </head>
-        <body>
-          <main style={{ maxWidth: 750, margin: "0 auto" }}>
-            {children}
-            <ScrollRestoration />
-            <Scripts />
-            <LiveReload />
-          </main>
-        </body>
-      </html>
-    );
-  }
-);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta name="emotion-insertion-point" content="emotion-insertion-point" />
+      </head>
+      <body>
+        <main style={{ maxWidth: 750, margin: "0 auto" }}>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </main>
+      </body>
+    </html>
+  );
+});
 
 // https://remix.run/api/conventions#default-export
 // https://remix.run/api/conventions#route-filenames
